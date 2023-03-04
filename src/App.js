c48f335f7f2f97988bd0bb15d218787ff29a465f
HarshGa387R <hgaur701@gmail.com>
2023-03-04 18:51:11 +0530

    creating text util app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar.mjs';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title={'Text-Util'}/>
+    </div>
+  );
+}
+
+export default App;
